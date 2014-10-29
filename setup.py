c5ac8e1ad8353b1e010316c1917528c325456525
Joel Porquet <joel@porquet.org>
2014-10-29 16:35:11 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
         requires = ["ConfigObj"],
         packages = ["jpass"],
         scripts = ["bin/jpass"],
-        data_files = [("share/jpass", ["docs/jpass.conf.example"])],
+        data_files = [("share/docs/jpass", ["docs/jpass.conf.example"])],
         )
