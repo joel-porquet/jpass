0394a56d410dff38263437cee2f09b01f60cbef0
Joel Porquet <joel@porquet.org>
2015-10-16 18:28:07 +0200

    add --xclip argument for copying in clipboard and make service into an optional positional argument

--- jpass/cli.py
+++ jpass/cli.py
@@ -1,6 +1,7 @@
 # Copyright 2014, Joel Porquet
 
 import argparse
+import subprocess
 import sys
 
 from .config import Config
@@ -36,25 +37,34 @@ def get_master_password(conf):
 def get_args():
         parser = argparse.ArgumentParser()
 
-        parser.add_argument("-v", "--verbosity", action="store_true",
-                help="increase verbosity level")
+        parser.add_argument('service', nargs='?', default=None,
+                help='specify a service name')
 
         parser.add_argument("-c", "--conf",
                 help="specify a configuration file (default is '{!s}')"
                 .format(Config.conf_file))
 
-        parser.add_argument("-s", "--service", help="specify a service name")
+        parser.add_argument("-x", "--xclip", action="store_true",
+                help="copy password to clipboard")
 
         parser.add_argument("-i", "--information", action="store_true",
                 help="only display information about a service")
 
+        parser.add_argument("-v", "--verbosity", action="store_true",
+                help="increase verbosity level")
+
         return parser.parse_args()
+
+def copy_clipboard(pwd):
+    with subprocess.Popen(["xclip"], stdin=subprocess.PIPE) as xclip:
+        xclip.stdin.write(pwd.encode("utf-8"))
+    print("Password copied to clipboard.")
+    pass
 
 def main():
     args = get_args()
 
-    conf = Config(args.conf, args.service,
-            args.verbosity, args.information)
+    conf = Config(args.conf, args.service, args.verbosity, args.information)
 
     service_name = get_service(conf)
     serv = Service(service_name, conf)
@@ -64,5 +74,9 @@ def main():
         master_pwd = get_master_password(conf)
         pwd = serv.generate_password(master_pwd)
 
-    serv.pretty_print(pwd)
+    if args.xclip:
+        serv.pretty_print(None)
+        copy_clipboard(pwd)
+    else:
+        serv.pretty_print(pwd)
 
